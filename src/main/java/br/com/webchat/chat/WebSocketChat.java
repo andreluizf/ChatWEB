69b0d0dd6131fd747068fa657c74a51c18337762
andre <ferreira.andreluiz@gmail.com>
2014-04-08 08:39:42 -0300

    correcoes

--- src/main/java/br/com/webchat/chat/WebSocketChat.java
+++ src/main/java/br/com/webchat/chat/WebSocketChat.java
@@ -1,9 +1,13 @@
 package br.com.webchat.chat;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import javax.ejb.Singleton;
+import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -11,54 +15,38 @@ import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/chat/{user}")
-@Singleton
+@ServerEndpoint("/chat")
 public class WebSocketChat {
-//   Set<Session> userSessions = Collections.synchronizedSet(new HashSet<Session>());
 
-    Map<String, Session> userSessions = Collections.synchronizedMap(new HashMap<String, Session>());
+    private static final Set<Session> peers
+            = Collections.synchronizedSet(new HashSet<Session>());
 
     @OnOpen
-    public void onOpen(Session userSession, @PathParam("user") String userName) {
-
-        System.out.println("user ::::: " + userName);
-
-        System.out.println("New request received. Id: " + userSession.getId());
-        userSessions.put(userName, userSession);
+    public void onOpen(Session peer) {
+        peers.add(peer);
     }
 
-    /**
-     * Callback hook for Connection close events.
-     *
-     * This method will be invoked when a client closes a WebSocket connection.
-     *
-     * @param userSession the userSession which is opened.
-     */
     @OnClose
-    public void onClose(Session userSession) {
-        System.out.println("Connection closed. Id: " + userSession.getId());
-        userSessions.remove(userSession);
+    public void onClose(Session peer) {
+        peers.remove(peer);
     }
 
-    /**
-     * Callback hook for Message Events.
-     *
-     * This method will be invoked when a client send a message.
-     *
-     * @param message The text message
-     * @param userSession The session of the client
-     */
     @OnMessage
-    public void onMessage(String message, @PathParam("user") String userName) {
-        System.out.println("Message Received: " + message);
-        System.out.println(userSessions.toString());
-        Session session = userSessions.get(userName);
-        System.out.println("Sending to " + session.getId());
-        session.getAsyncRemote().sendText(message);
-//        for (Session session : userSessions) {
-//            System.out.println("Sending to " + session.getId());
-//            session.getAsyncRemote().sendText(message);
-//        }
+    public void message(String message, Session client)
+            throws IOException, EncodeException {
+        System.out.println("Message Received: " + client.getOpenSessions().toString());
+
+        if (message.contains(" ::user")) {
+            for (Session peer : client.getOpenSessions()) {
+                peer.getBasicRemote().sendText(message);
+            }
+        } else {
+            for (Session peer : client.getOpenSessions()) {
+                peer.getBasicRemote().sendText(message);
+            }
+
+        }
+
     }
 
 }
--- src/main/webapp/js/websocket.js
+++ src/main/webapp/js/websocket.js
@@ -1,49 +1,20 @@
-
-var wsUri;
-
-
+var wsUri = "ws://" + document.location.host + document.location.pathname + "chat";
+console.log(wsUri);
+var websocket = new WebSocket(wsUri);
 
 var username;
-var nome;
-var cidade;
-var nacimento;
-var frase;
-var apelido;
-var websocket;
-
+websocket.onopen = function(evt) { onOpen(evt) };
+websocket.onmessage = function(evt) { onMessage(evt) };
+websocket.onerror = function(evt) { onError(evt) };
 var output = document.getElementById("output");
+
 function join() {
-
-    wsUri = "ws://" + document.location.host + document.location.pathname + "chat/" + textField.value;
-    websocket = new WebSocket(wsUri);
-    websocket.onopen = function(username, evt) {
-
-        onOpen(evt)
-    };
-    websocket.onmessage = function(evt) {
-        onMessage(evt)
-    };
-    websocket.onerror = function(evt) {
-        onError(evt)
-    };
-    
-//    nome = textNome.value;
-//    cidade = textCidade.value;
-//    nacimento = textNascimento.value;
-//    frase = textFrase.value;
-//    apelido = textApelido.value;
-
     username = textField.value;
-    setTimeout(function(){
-        websocket.send(username + " joined");
-    },1000)
-    
+    websocket.send(username + " ::user");
 }
 
 function send_message() {
-    websocket.send("Data:" + new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + " - " + username + ": " + textField.value);
-
-
+    websocket.send(username + ": " + textField.value);
 }
 
 function onOpen() {
@@ -52,8 +23,14 @@ function onOpen() {
 
 function onMessage(evt) {
     console.log("onMessage: " + evt.data);
-    if (evt.data.indexOf("joined") != -1) {
-        userField.innerHTML += evt.data.substring(0, evt.data.indexOf(" joined")) + "\n";
+    if (evt.data.indexOf("::user") != -1) {
+        var html='<p><div class="dropdown">'
+                  +'<a data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> '+evt.data.substring(0, evt.data.indexOf(" ::user"))+'</a>'
+                  +'<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel" >'
+                  +'<p style="margin-left: 5px"><a href="#" id="1" class="conversa" ><span class="glyphicon glyphicon-phone"></span> Iniciar Conversa</a></p>'
+                  +'<p style="margin-left: 5px"><a href="#"  data-toggle="modal" data-target="#modal-info"><span class="glyphicon glyphicon-info-sign"></span> Informações</a></p>'
+                  +'</ul></div></p>';
+        tabs1.innerHTML += html;
     } else {
         chatlogField.innerHTML += evt.data + "\n";
     }
@@ -66,5 +43,75 @@ function onError(evt) {
 function writeToScreen(message) {
     output.innerHTML += message + "<br>";
 }
-
-
+//
+//
+////
+//var wsUri;
+//
+//
+//
+//var username;
+//var nome;
+//var cidade;
+//var nacimento;
+//var frase;
+//var apelido;
+//var websocket;
+//
+//var output = document.getElementById("output");
+//function join() {
+//
+//    wsUri = "ws://" + document.location.host + document.location.pathname + "chat/" + textField.value;
+//    websocket = new WebSocket(wsUri);
+//    websocket.onopen = function(username, evt) {
+//
+//        onOpen(evt)
+//    };
+//    websocket.onmessage = function(evt) {
+//        onMessage(evt)
+//    };
+//    websocket.onerror = function(evt) {
+//        onError(evt)
+//    };
+//    
+////    nome = textNome.value;
+////    cidade = textCidade.value;
+////    nacimento = textNascimento.value;
+////    frase = textFrase.value;
+////    apelido = textApelido.value;
+//
+//    username = textField.value;
+//    setTimeout(function(){
+//        websocket.send(username + " joined");
+//    },1000)
+//    
+//}
+//
+//function send_message() {
+//    websocket.send("Data:" + new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + " - " + username + ": " + textField.value);
+//
+//
+//}
+//
+//function onOpen() {
+//    writeToScreen("Connected to " + wsUri);
+//}
+//
+//function onMessage(evt) {
+//    console.log("onMessage: " + evt.data);
+//    if (evt.data.indexOf("joined") != -1) {
+//        userField.innerHTML += evt.data.substring(0, evt.data.indexOf(" joined")) + "\n";
+//    } else {
+//        chatlogField.innerHTML += evt.data + "\n";
+//    }
+//}
+//
+//function onError(evt) {
+//    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+//}
+//
+//function writeToScreen(message) {
+//    output.innerHTML += message + "<br>";
+//}
+//
+//
